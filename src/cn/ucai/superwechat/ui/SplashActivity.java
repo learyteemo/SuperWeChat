a742f3175d5ca801f0f15d34d41f72451ab9a552
learyteemo <1604585139@qq.com>
2016-11-01 13:27:13 +0800

    闪屏功能实现

--- src/cn/ucai/superwechat/ui/SplashActivity.java
+++ src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -23,14 +23,7 @@ public class SplashActivity extends BaseActivity {
 		setContentView(R.layout.em_activity_splash);
 		super.onCreate(arg0);
 
-		RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
-		TextView versionText = (TextView) findViewById(R.id.tv_version);
-		versionText.setText(getVersion());
-		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
-		animation.setDuration(1500);
-		rootLayout.startAnimation(animation);
 	}
-
 	@Override
 	protected void onStart() {
 		super.onStart();
@@ -59,7 +52,7 @@ public class SplashActivity extends BaseActivity {
 						Thread.sleep(sleepTime);
 					} catch (InterruptedException e) {
 					}
-					startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+					startActivity(new Intent(SplashActivity.this, GuideActivity.class));
 					finish();
 				}
 			}
