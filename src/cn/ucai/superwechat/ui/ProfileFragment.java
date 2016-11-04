f9b41768195c289e341b362ae09b39a75176c865
learyteemo <1604585139@qq.com>
2016-11-04 12:55:48 +0800

    编写fragment_profile布局

---
+++ src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -0,0 +1,92 @@
+package cn.ucai.superwechat.ui;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.easemob.redpacketui.utils.RedPacketUtil;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    @Bind(R.id.iv_profile_avatar)
+    ImageView mivProfileAvatar;
+    @Bind(R.id.tv_profile_nickname)
+    TextView mtvProfileNickname;
+    @Bind(R.id.tv_profile_username)
+    TextView mtvProfileUsername;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
+            return;
+        setUserInfo();
+    }
+
+    private void setUserInfo() {
+        EaseUserUtils.setCurrentAppUserAvatar(getActivity(),mivProfileAvatar);
+        EaseUserUtils.setCurrentAppUserNick(mtvProfileNickname);
+        EaseUserUtils.setCurrentAppUserName(mtvProfileUsername);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.layout_profile_view, R.id.tv_profile_money, R.id.tv_profile_settings})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_profile_view:
+                break;
+            //red packet code : 进入零钱页面
+            case R.id.tv_profile_money:
+                RedPacketUtil.startChangeActivity(getActivity());
+                break;
+            //end of red packet code
+            case R.id.tv_profile_settings:
+                break;
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(((MainActivity)getActivity()).isConflict){
+            outState.putBoolean("isConflict", true);
+        }else if(((MainActivity)getActivity()).getCurrentAccountRemoved()){
+            outState.putBoolean(Constant.ACCOUNT_REMOVED, true);
+        }
+    }
+}
