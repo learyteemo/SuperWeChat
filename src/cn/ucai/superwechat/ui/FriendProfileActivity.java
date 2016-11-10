ab50b27e680c3cf3f5e3e7b5f56006a4f6e6aed7
learyteemo <1604585139@qq.com>
2016-11-10 11:28:55 +0800

    好友详情列表实现视频通话

--- src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
@@ -87,6 +89,14 @@ public class FriendProfileActivity extends BaseActivity {
                 MFGT.gotoChat(this,user.getMUserName());
                 break;
             case R.id.btn_send_video:
+                if (!EMClient.getInstance().isConnected())
+                    Toast.makeText(this, R.string.not_connect_to_server, Toast.LENGTH_SHORT).show();
+                else {
+                    startActivity(new Intent(this, VideoCallActivity.class).putExtra("username", user.getMUserName())
+                            .putExtra("isComingCall", false));
+                    // videoCallBtn.setEnabled(false);
+                  //  inputMenu.hideExtendMenuContainer();
+                }
                 break;
             case R.id.btn_add_contact:
                 MFGT.gotoAddFriendMsg(this, user.getMUserName());
